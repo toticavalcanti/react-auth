138dc7efd05c1bf20c8f2deefaadf68254391143
toticavalcanti <toticavalcanti@hotmail.com>
2023-10-06 16:43:15 -0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -4,22 +4,17 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <form className='form-floating'>
+      <h1 className="h3 mb-3 fw-normal">Please sign in</h1>
+      <div className="form-signin">
+          <input type="email" className="form-control" placeholder="name@example.com" required />
+      </div>
+      <div className="form-signin">
+          <input type="password" className="form-control" placeholder="Password" required />
+      </div>
+      <button className="form-signin btn btn-primary w-100 py-2" type="submit">Sign in</button>
+      <p className="mt-5 mb-3 text-body-secondary">&copy; 2017–2023</p>
+    </form>
   );
 }
 
