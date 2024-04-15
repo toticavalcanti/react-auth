7a4f10f801069c2681984ae42ec5417c0ed015bd
toticavalcanti <toticavalcanti@hotmail.com>
2024-04-15 20:28:06 -0300

    user authentication

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,25 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
 const Home: React.FC = () => {
-    return (
-      <div className="container">
-        <h1>You are not logged in!</h1>
-      </div>
-    );
-  }
-  
-  export default Home;
+  const [message, setMessage] = useState('');
+
+  useEffect(() => {
+    (async () => {
+      try {
+        const response = await axios.get('user');
+        const user = response.data;
+        setMessage(`Hi ${user.first_name} ${user.last_name}`);
+      } catch (e) {
+        setMessage('You are not logged in!');
+      }
+    })();
+  }, []);
+  return (
+    <div className="container">
+      <h1>{message}</h1>
+    </div>
+  );
+}
+
+export default Home;
